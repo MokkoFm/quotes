fa2e48e8afbaed6e9ba0ed7ca6ef781913b804e9
MokkoFm <mokkofmpoetry@gmail.com>
2021-01-29 22:47:22 +0200

    add working changeQuote button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import './App.css';
 
+const quotes = [
+  {
+    quote: "The question isn’t who is going to let me; it’s who is going to stop me",
+    author: 'Benjamin Franklin',
+  },
+  {
+    quote: "I didn’t fail the test. I just found 100 ways to do it wrong.",
+    author: 'Benjamin Franklin',
+  },
+  {
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    author: "Napoleon Hill"
+  },
+  {
+    quote: "It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.",
+    author: "Mae Jamison"
+  },
+]
+
+let index = 1;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       quote: "The question isn’t who is going to let me; it’s who is going to stop me",
-      author: 'Benjamin Franklin'
+      author: 'Benjamin Franklin',
     }
+  }
+  changeQuote() {
+    while (index < quotes.length) {
+      this.setState({
+        quote: quotes[index].quote,
+        author: quotes[index].author,
+      })
+    }
+    index++;
   }
   render() {
     return (
@@ -23,7 +52,7 @@ class App extends React.Component {
             <a id="fb-quote"><i class="fab fa-facebook-f"></i></a>
           </div>
           <div className="submit">
-            <button id="new-quote">New quote</button>
+            <button id="new-quote" onClick={this.changeQuote.bind(this)}>New quote</button>
           </div>
         </div>
       </div>
